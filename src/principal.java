99fde03107ba21cc181ad49c11fae2b94ddda793
LAB03-PC08 <LAB03-PC08@LAB03-PC08>
2016-11-03 15:08:10 +0100

    leo

--- src/principal.java
+++ src/principal.java
@@ -12,5 +12,6 @@ public class principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("prueba1");
+        System.out.println("lkasdjlads");
     }
 }
